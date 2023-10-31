413a938a0e01cf5f584d0b9f3c1672abebb9c9ff
abdelrahman <https://github.com/abdo6999>
2023-10-31 16:57:45 +0200

    anim test

---
+++ images.js
@@ -0,0 +1,58 @@
+const mockData = [
+  {
+    name: 'John Doe',
+    location: 'New York, USA',
+    image:"./images/image 1.png",
+    description: 'Experienced web developer with a passion for creating user-friendly and responsive web applications.'
+  },
+  {
+    name: 'Alice Smith',
+    location: 'London, UK',
+    image:"./images/image 2.png",
+    description: 'Front-end developer specializing in creating beautiful and interactive user interfaces.'
+  },
+  {
+    name: 'Ella Johnson',
+    location: 'San Francisco, USA',
+    image:"./images/image 3.png",
+    description: 'Full-stack developer with expertise in building scalable and efficient web solutions.'
+  },
+  {
+    name: 'Michael Brown',
+    location: 'Sydney, Australia',
+    image:"./images/image 4.png",
+    description: 'UI/UX designer focused on crafting intuitive and visually appealing user experiences.'
+  },
+  {
+    name: 'Sarah Wilson',
+    location: 'Toronto, Canada',
+    image:"./images/image 5.png",
+    description: 'Back-end developer with a strong background in database design and optimization.'
+  },
+  {
+    name: 'David Lee',
+    location: 'Berlin, Germany',
+    image:"./images/image 6.png",
+    description: 'Mobile app developer with a knack for creating cross-platform applications that run smoothly.'
+  },
+  {
+    name: 'Olivia Davis',
+    location: 'Los Angeles, USA',
+    image:"./images/image 7.png",
+    description: 'Graphic designer specializing in branding and visual identity design.'
+  },
+  {
+    name: 'James Wilson',
+    location: 'Paris, France',
+    image:"./images/image 8.png",
+    description: 'Software engineer with a passion for open-source projects and community-driven development.'
+  },
+  {
+    name: 'Sophia Anderson',
+    location: 'Tokyo, Japan',
+    image:"./images/image 9.png",
+    description: 'Front-end developer with expertise in modern web technologies and performance optimization.'
+  }
+];
+
+export default mockData
---
+++ script.js
@@ -0,0 +1,148 @@
+import mockData from "./images.js";
+
+// Get the container for carousel items and create a controller div
+const carousel = document.getElementById("carousel-items");
+const selectedItem = document.getElementById("selected-item");
+let isMouseDown = false;
+let isDragging = false; 
+let startX, scrollLeft;
+
+function initView(mockData) {
+  for (let i = 1; i < mockData.length; i++) {
+    const item = createItem(i)
+    carousel.appendChild(item);
+  }
+  const item = createItem(0)
+  item.children[2].classList.add("show")
+  selectedItem.appendChild(item)
+}
+
+initView(mockData)
+
+let timeOutId
+
+
+carousel.addEventListener("click", (e) => {
+  const item = e.target;
+  if (item.classList.contains("carousel-item")&& !isDragging) {
+    clearTimeout(timeOutId);
+    const newItem = item.cloneNode(true)
+    const rect = item.getBoundingClientRect();
+    newItem.style.setProperty('--start-top', `${rect.top}px`);
+    newItem.style.setProperty('--start-left', `${rect.left}px`);
+    newItem.classList.add("selected")
+    setTimeout(() => {
+      item.classList.add("selected")
+    }, 100);
+    selectedItem.appendChild(newItem)
+    setTimeout(() => {
+      newItem.children[2].classList.add("hide")
+    }, 500);
+    setTimeout(() => {
+      newItem.children[2].classList.remove("hide")
+      newItem.children[2].classList.add("show")
+    }, 1500);
+    setTimeout(() => {
+      item.classList.remove("selected")
+      carousel.appendChild(item)
+    }, 1500);
+    timeOutId = setTimeout(() => {
+      const selectedItems = selectedItem.children
+      for (let i = 0; i < selectedItems.length; i++) {
+        if(i !== selectedItems.length - 1){
+          selectedItems[i].remove()
+        }
+      }
+    }, 1500);
+  }
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+carousel.addEventListener("mousedown", (e) => {
+  isMouseDown = true;
+  startX = e.pageX - carousel.offsetLeft;
+  scrollLeft = carousel.scrollLeft;
+  isDragging = false; 
+});
+
+carousel.addEventListener("mouseleave", () => {
+  isMouseDown = false;
+  isDragging = false;
+});
+
+carousel.addEventListener("mouseup", (e) => {
+  isMouseDown = false;
+  const nearestSnapPoint = Math.round(carousel.scrollLeft/250 % 250) * 260
+  carousel.scrollTo({
+    left: nearestSnapPoint,
+    behavior: "smooth",
+  })
+});
+
+carousel.addEventListener("mousemove", (e) => {
+  if (!isMouseDown) return;
+  e.preventDefault();
+  const x = e.pageX - carousel.offsetLeft;
+  const walk = (x - startX) * 1;  
+  carousel.scrollLeft = scrollLeft - walk;
+  isDragging = true;
+});
+
+
+
+
+
+
+function createItem(i) {
+  // Create elements for each item
+  const item = document.createElement("div");
+  item.className = "carousel-item";
+  const overlay = document.createElement("div");
+  overlay.className = "overlay";
+
+  const image = document.createElement("img");
+  image.src = mockData[i].image;
+
+  const itemContent = document.createElement("div");
+  itemContent.className = "carousel-content";
+
+  const location = document.createElement("p");
+  location.textContent = mockData[i].location;
+  location.className = "location";
+
+  const name = document.createElement("h1");
+  name.textContent = mockData[i].name;
+  name.className = "name";
+
+  const description = document.createElement("p");
+  description.textContent = mockData[i].description;
+  description.className = "text-hide description";
+
+  // Append elements to the thumbnail item
+  itemContent.appendChild(location);
+  itemContent.appendChild(name);
+  itemContent.appendChild(description);
+  item.appendChild(overlay);
+  item.appendChild(image);
+  item.appendChild(itemContent);
+
+  return item
+}
+
+
